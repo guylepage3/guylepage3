023e3ec360aab818d584a5bddc07503a38cdf2fc
Guy Lepage <guylepage3@gmail.com>
2023-04-07 07:42:01 -0700

    resolved camelCase json
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- src/js/index.js
+++ src/js/index.js
@@ -4,19 +4,19 @@ async function loadRankStats() {
   );
   const data = await response.json();
   const starTotal = data.userStats.directStars + data.userStats.indirectStars;
-  const morningCommits = (
+  const getMorningCommits = (
     (30.78250863061 / 100) *
     data.userStats.commits
   ).toFixed(0);
-  const lateMorningCommits = (
+  const getLateMorningCommits = (
     (45.265493999671 / 100) *
     data.userStats.commits
   ).toFixed(0);
-  const afternoonCommits = (
+  const getAfternoonCommits = (
     (21.477889199408 / 100) *
     data.userStats.commits
   ).toFixed(0);
-  const eveningCommits = (
+  const getEveningCommits = (
     (2.4741081703107 / 100) *
     data.userStats.commits
   ).toFixed(0);
@@ -24,26 +24,26 @@ async function loadRankStats() {
   const rankDescriptor = (100 - dataScoreRank).toFixed(2);
   
   const lastFetch = data.userStats.lastFetch 
-  const fetchDateTime = lastFetch.replace("T", " ").slice(0, -14);
+  const fetchLastFetch = lastFetch.replace("T", " ").slice(0, -14);
 
   var obj = {};
   obj.rank = data.level;
-  obj.rank_percent = dataScoreRank + " %";
-  obj.rank_descriptor = rankDescriptor + " %";
+  obj.rankPercent = dataScoreRank + " %";
+  obj.topRankPercent = rankDescriptor + " %";
   obj.score = parseInt(data.score, 10).toLocaleString();
-  obj.score_num = parseInt(data.score, 10).toLocaleString() + " / 300,000";
-  obj.score_percent = dataScoreRank + " %";
+  obj.scoreCalc = parseInt(data.score, 10).toLocaleString() + " / 300,000";
+  obj.scorePercent = dataScoreRank + " %";
   obj.stars = starTotal.toLocaleString();
   obj.commits = data.userStats.commits.toLocaleString();
-  obj.pull_requests = data.userStats.pullRequests.toLocaleString();
+  obj.pullRequests = data.userStats.pullRequests.toLocaleString();
   obj.issues = data.userStats.issues.toLocaleString();
-  obj.contributed_to = data.userStats.contributedTo.toLocaleString();
+  obj.contributedTo = data.userStats.contributedTo.toLocaleString();
   obj.followers = data.userStats.followers.toLocaleString();
-  obj.date_time_fetch = fetchDateTime;
-  obj.morning = parseInt(morningCommits, 10).toLocaleString();
-  obj.late_morning = parseInt(lateMorningCommits, 10).toLocaleString();
-  obj.afternoon = parseInt(afternoonCommits, 10).toLocaleString();
-  obj.evening = parseInt(eveningCommits, 10).toLocaleString();
+  obj.fetchDateTime = fetchLastFetch;
+  obj.morningCommits = parseInt(getMorningCommits, 10).toLocaleString();
+  obj.lateMorningCommits = parseInt(getLateMorningCommits, 10).toLocaleString();
+  obj.afternoonCommits = parseInt(getAfternoonCommits, 10).toLocaleString();
+  obj.eveningCommits = parseInt(getEveningCommits, 10).toLocaleString();
 
   document.getElementById("json").innerHTML = JSON.stringify(obj, undefined, 2);
 }
