6cccf36edd53fe0346f6693a1a5c63fe71d52de6
Guy Lepage <guylepage3@gmail.com>
2023-05-13 10:11:39 -0700

    added year in progress
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- src/js/index.js
+++ src/js/index.js
@@ -47,6 +47,7 @@ async function loadRankStats() {
   obj.contributed_to = data.userStats.contributedTo.toLocaleString();
   obj.followers = data.userStats.followers.toLocaleString();
   obj.days_left = (365-Math.round(daysLeft)) + ":" + Math.round(daysLeft);
+  obj.years_progress = (365/Math.round(daysLeft)) + " %";
   obj.fetch_date_time = fetchLastFetch;
   obj.morning_commits = parseInt(getMorningCommits, 10).toLocaleString();
   obj.lateMorning_commits = parseInt(getLateMorningCommits, 10).toLocaleString();
